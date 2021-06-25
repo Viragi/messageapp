a08050d164cd11cc939a5de32baed4a152519b2f
Viragi Shah <viragi311@gmail.com>
2021-06-25 03:27:33 -0700

    seprating logic

--- src/useGiphy.js
+++ src/useGiphy.js
@@ -4,7 +4,7 @@ export default function useGiphy(){
     const [gifData, setGifData] = useState([]);
 
     useEffect(() => {
-      fetch(`http://api.giphy.com/v1/gifs/trending?api_key=Awq5410QQl0416nJogqlsinldM2s9PCA`).then(res => res.json()).then((res) => {
+      fetch(`http://api.giphy.com/v1/gifs/trending?api_key=RGg1PKJeFM6XYAmzmv2BHhzeCjhEU6lg`).then(res => res.json()).then((res) => {
         setGifData(res.data);
       }).catch((e)=> console.error(e, "error"))
     },[])
